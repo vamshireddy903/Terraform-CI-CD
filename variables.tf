c8a909d2639e77f6b2294239ddca5f52a7bc5132
Vamsi <devopsaws925@gmail.com>
2025-06-09 22:09:59 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "ami_value" {
+    description = "value for ami"
+    type        = string
+}
+
+variable "instance_type_value" {
+    description = "value for instance type"
+    type        = string    
+  
+}
