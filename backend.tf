1c249e26690a50249f8a8b92edf4cc8720ab03da
Vamsi <devopsaws925@gmail.com>
2025-06-09 22:51:14 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "my-unique-bucket-vamsi-1997"
-    dynamodb_table = "terraform-state-lock-dynamo"
-    key    = "Locking/terraform.tfstate"
-    encrypt = true
-    region = "us-east-1"
-  }
-}
